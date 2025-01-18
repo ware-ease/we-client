49fbef7ed69f23c737f8c6ff0af167e0eaa5d419
conboaduide <vuvananhben26@gmail.com>
2025-01-19 02:01:48 +0700

    add warehouses page

--- app/[locale]/(console)/settings/page.tsx
+++ app/[locale]/(console)/settings/page.tsx
@@ -1,5 +1,5 @@
 const Settings = () => {
-  return <div>Settings</div>;
+  return <div>Hello</div>;
 };
 
 export default Settings;
---
+++ app/[locale]/(console)/warehouses/page.tsx
@@ -0,0 +1,5 @@
+const Warehouses = () => {
+  return <div>Warehouses</div>;
+};
+
+export default Warehouses;
