f6aae6e615eb11316785e7d5ff197e392c4a961c
wintangz <wintang2003@icloud.com>
2025-01-19 15:47:20 +0700

    fix useIsMobile rendering issues

--- lib/useIsMobile.tsx
+++ lib/useIsMobile.tsx
@@ -1,37 +1,38 @@
+'use client';
 import * as React from 'react';
 
 const useIsMobile = (mobileScreenSize: number = 768): boolean => {
-  if (typeof window.matchMedia !== 'function') {
-    throw Error('matchMedia not supported by browser!');
-  }
-
-  const [isMobile, setIsMobile] = React.useState<boolean>(
-    window.matchMedia(`(max-width: ${mobileScreenSize}px)`).matches
-  );
-
-  const checkIsMobile = React.useCallback((event: MediaQueryListEvent) => {
-    setIsMobile(event.matches);
-  }, []);
+  const [isMobile, setIsMobile] = React.useState<boolean>(false);
 
   React.useEffect(() => {
+    if (typeof window.matchMedia !== 'function') {
+      console.error('matchMedia is not supported by the browser!');
+      return;
+    }
+
     const mediaListener = window.matchMedia(
       `(max-width: ${mobileScreenSize}px)`
     );
+    setIsMobile(mediaListener.matches);
+
+    const checkIsMobile = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches);
+    };
 
     try {
       mediaListener.addEventListener('change', checkIsMobile);
     } catch {
-      mediaListener.addListener(checkIsMobile);
+      mediaListener.addListener(checkIsMobile); // For older browsers
     }
 
     return () => {
       try {
         mediaListener.removeEventListener('change', checkIsMobile);
       } catch {
-        mediaListener.removeListener(checkIsMobile);
+        mediaListener.removeListener(checkIsMobile); // For older browsers
       }
     };
-  }, [mobileScreenSize, checkIsMobile]);
+  }, [mobileScreenSize]);
 
   return isMobile;
 };
