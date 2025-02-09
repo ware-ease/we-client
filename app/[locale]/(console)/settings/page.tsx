76fa7d379c537831eadf0a14580fc61299d229ab
wintangz <wintang2003@icloud.com>
2025-02-10 01:56:48 +0700

    fix ui of the setting page

--- app/[locale]/(console)/settings/page.tsx
+++ app/[locale]/(console)/settings/page.tsx
@@ -100,153 +100,159 @@ const Settings = () => {
           <div>{t('Settings.title')}</div>
         </div>
         <div className='flex flex-col p-4 gap-6 max-h-[80vh] overflow-y-auto'>
-          <div className='flex flex-col border-2 rounded-md w-3/4'>
-            <div className='p-4 text-xl text-primary border-b-2'>
-              <div>{t('Settings.profile')}</div>
-            </div>
-            <div className='p-4 flex flex-col gap-3'>
-              <div className='flex'>
-                <div className='flex items-center w-1/5'>
-                  {t('Settings.avatar')}
-                </div>
-                <Avatar className='size-16 hover:cursor-pointer'>
-                  <AvatarImage src={avatar} />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div className='flex flex-col justify-center ml-6'>
-                  <Dialog onOpenChange={handleAvatarChangeClick}>
-                    <DialogTrigger asChild>
-                      <div className='hover:font-semibold hover:cursor-pointer'>
-                        {t('Settings.change')}
+          <div className='flex border-2 rounded-md w-full'>
+            <div className='flex-col w-full'>
+              <div className='p-4 text-xl text-primary border-b-2'>
+                <div>{t('Settings.profile')}</div>
+              </div>
+              <div className='flex justify-between relative'>
+                <div className='w-3/4 p-4 flex flex-col gap-3'>
+                  <div className='flex'>
+                    <div className='flex items-center w-1/5'>
+                      {t('Settings.lastname')}
+                    </div>
+                    <Input className='border-gray-300 w-2/5' />
+                  </div>
+                  <div className='flex'>
+                    <div className='flex items-center w-1/5'>
+                      {t('Settings.firstname')}
+                    </div>
+                    <Input className='border-gray-300 w-2/5' />
+                  </div>
+                  <div className='flex'>
+                    <div className='flex items-center w-1/5'>
+                      {t('Settings.email')}
+                    </div>
+                    <Input className='border-gray-300 w-2/5' />
+                  </div>
+                  <div className='flex'>
+                    <div className='flex items-center w-1/5'>
+                      {t('Settings.phone')}
+                    </div>
+                    <Input className='border-gray-300 w-2/5' />
+                  </div>
+                  <div className='flex'>
+                    <div className='flex items-center w-1/5'>
+                      {t('Settings.address')}
+                    </div>
+                    <Input className='border-gray-300 w-2/5' />
+                  </div>
+                  <div className='flex py-2'>
+                    <div className='flex items-center w-1/5'>
+                      {t('Settings.sex')}
+                    </div>
+                    <RadioGroup className='flex' defaultValue='option-one'>
+                      <div className='flex items-center space-x-2'>
+                        <RadioGroupItem
+                          className='fill-blue-500'
+                          value='option-one'
+                          id='option-one'
+                        />
+                        <Label htmlFor='option-one'>Nam</Label>
                       </div>
-                    </DialogTrigger>
-                    <DialogContent className='max-w-md'>
-                      <DialogHeader>
-                        <DialogTitle>{t('Settings.upload')}</DialogTitle>
-                        <DialogDescription>
-                          {t('Settings.choose')}
-                        </DialogDescription>
-                      </DialogHeader>
-                      {preview && (
-                        <div className='flex justify-center my-2'>
-                          <Avatar className='size-40'>
-                            <AvatarImage src={preview} />
-                            <AvatarFallback>CN</AvatarFallback>
-                          </Avatar>
+                      <div className='flex items-center space-x-2'>
+                        <RadioGroupItem
+                          className=''
+                          value='option-two'
+                          id='option-two'
+                        />
+                        <Label htmlFor='option-two'>Nữ</Label>
+                      </div>
+                    </RadioGroup>
+                  </div>
+                  <div className='flex'>
+                    <div className='flex items-center w-1/5'>
+                      {t('Settings.nation')}
+                    </div>
+                    <Input className='border-gray-300 w-2/5' />
+                  </div>
+                  <div className='w-3/5 text-right'>
+                    <Button className='bg-green-700 hover:bg-green-950'>
+                      {t('Settings.update')}
+                    </Button>
+                  </div>
+                </div>
+                <div className='flex flex-col w-1/4 absolute right-20 py-4 gap-3'>
+                  <div className='flex items-center font-semibold'>
+                    {t('Settings.avatar')}
+                  </div>
+                  <Avatar className='size-48 hover:cursor-pointer'>
+                    <AvatarImage src={avatar} />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <div className='flex flex-col justify-center'>
+                    <Dialog onOpenChange={handleAvatarChangeClick}>
+                      <DialogTrigger asChild>
+                        <div className='hover:font-semibold hover:cursor-pointer'>
+                          {t('Settings.change')}
                         </div>
-                      )}
-                      <Input
-                        className='w-96'
-                        type='file'
-                        accept='image/*'
-                        onChange={handleAvatarUpload}
-                      />
-                      <DialogFooter className='justify-end'>
-                        <DialogClose asChild>
-                          <Button variant='secondary'>
-                            {t('Settings.cancel')}
-                          </Button>
-                        </DialogClose>
-                        <DialogClose asChild>
-                          <Button onClick={handleAvatarChangeConfirm}>
-                            {t('Settings.update')}
-                          </Button>
-                        </DialogClose>
-                      </DialogFooter>
-                    </DialogContent>
-                  </Dialog>
-                  <div className='hover:font-bold hover:cursor-pointer text-red-500 font-semibold'>
-                    {t('Settings.remove')}
+                      </DialogTrigger>
+                      <DialogContent className='max-w-md'>
+                        <DialogHeader>
+                          <DialogTitle>{t('Settings.upload')}</DialogTitle>
+                          <DialogDescription>
+                            {t('Settings.choose')}
+                          </DialogDescription>
+                        </DialogHeader>
+                        {preview && (
+                          <div className='flex justify-center my-2'>
+                            <Avatar className='size-40'>
+                              <AvatarImage src={preview} />
+                              <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                          </div>
+                        )}
+                        <Input
+                          className='w-96'
+                          type='file'
+                          accept='image/*'
+                          onChange={handleAvatarUpload}
+                        />
+                        <DialogFooter className='justify-end'>
+                          <DialogClose asChild>
+                            <Button variant='secondary'>
+                              {t('Settings.cancel')}
+                            </Button>
+                          </DialogClose>
+                          <DialogClose asChild>
+                            <Button onClick={handleAvatarChangeConfirm}>
+                              {t('Settings.update')}
+                            </Button>
+                          </DialogClose>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+                    <div className='hover:font-bold hover:cursor-pointer text-red-500 font-semibold'>
+                      {t('Settings.remove')}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className='flex'>
-                <div className='flex items-center w-1/5'>
-                  {t('Settings.lastname')}
-                </div>
-                <Input className='border-gray-300 w-2/5' />
-              </div>
-              <div className='flex'>
-                <div className='flex items-center w-1/5'>
-                  {t('Settings.firstname')}
-                </div>
-                <Input className='border-gray-300 w-2/5' />
-              </div>
-              <div className='flex'>
-                <div className='flex items-center w-1/5'>
-                  {t('Settings.email')}
-                </div>
-                <Input className='border-gray-300 w-2/5' />
-              </div>
-              <div className='flex'>
-                <div className='flex items-center w-1/5'>
-                  {t('Settings.phone')}
-                </div>
-                <Input className='border-gray-300 w-2/5' />
-              </div>
-              <div className='flex'>
-                <div className='flex items-center w-1/5'>
-                  {t('Settings.address')}
-                </div>
-                <Input className='border-gray-300 w-2/5' />
-              </div>
-              <div className='flex py-2'>
-                <div className='flex items-center w-1/5'>
-                  {t('Settings.sex')}
-                </div>
-                <RadioGroup className='flex' defaultValue='option-one'>
-                  <div className='flex items-center space-x-2'>
-                    <RadioGroupItem
-                      className='fill-blue-500'
-                      value='option-one'
-                      id='option-one'
-                    />
-                    <Label htmlFor='option-one'>Nam</Label>
-                  </div>
-                  <div className='flex items-center space-x-2'>
-                    <RadioGroupItem
-                      className=''
-                      value='option-two'
-                      id='option-two'
-                    />
-                    <Label htmlFor='option-two'>Nữ</Label>
-                  </div>
-                </RadioGroup>
-              </div>
-              <div className='flex'>
-                <div className='flex items-center w-1/5'>
-                  {t('Settings.nation')}
-                </div>
-                <Input className='border-gray-300 w-2/5' />
-              </div>
-              <div className='w-3/5 text-right'>
-                <Button className='bg-green-700 hover:bg-green-950'>
-                  {t('Settings.update')}
-                </Button>
               </div>
             </div>
           </div>
-          <div className='flex flex-col border-2 rounded-md w-3/4'>
-            <div className='p-4 text-xl text-primary border-b-2'>
-              <div>{t('Settings.account')}</div>
-            </div>
-            <div className='p-4 flex flex-col gap-3'>
-              <div className='flex'>
-                <div className='flex items-center w-1/5'>
-                  {t('Login.username')}
-                </div>
-                <Input className='border-gray-300 w-2/5' />
+          <div className='flex border-2 rounded-md w-full'>
+            <div className='flex-col w-full'>
+              <div className='p-4 text-xl text-primary border-b-2'>
+                <div>{t('Settings.account')}</div>
               </div>
-              <div className='flex'>
-                <div className='flex items-center w-1/5'>
-                  {t('Login.password')}
+              <div className='p-4 flex flex-col gap-3 w-3/4'>
+                <div className='flex'>
+                  <div className='flex items-center w-1/5'>
+                    {t('Login.username')}
+                  </div>
+                  <Input className='border-gray-300 w-2/5' />
                 </div>
-                <Input type='password' className='border-gray-300 w-2/5' />
-              </div>
-              <div className='w-3/5 text-right'>
-                <Button className='bg-green-700 hover:bg-green-950'>
-                  {t('Settings.update')}
-                </Button>
+                <div className='flex'>
+                  <div className='flex items-center w-1/5'>
+                    {t('Login.password')}
+                  </div>
+                  <Input type='password' className='border-gray-300 w-2/5' />
+                </div>
+                <div className='w-3/5 text-right'>
+                  <Button className='bg-green-700 hover:bg-green-950'>
+                    {t('Settings.update')}
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
