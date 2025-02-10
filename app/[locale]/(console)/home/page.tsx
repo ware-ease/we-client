f3c3f7435987ea1556f4fc82a301e5fb11f315ef
wintangz <wintang2003@icloud.com>
2025-02-10 23:44:54 +0700

    fix height of home page components

--- app/[locale]/(console)/home/page.tsx
+++ app/[locale]/(console)/home/page.tsx
@@ -31,7 +31,7 @@ const Home = () => {
               </h2>
               <div className='grid grid-cols-3 gap-4'>
                 <Link href={`/${language}/warehouse`} className='group'>
-                  <div className='bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
+                  <div className='h-full bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
                     <Box className='text-red-500 w-6 h-6' />
                     <div>
                       <h2 className='font-medium group-hover:underline'>
@@ -45,7 +45,7 @@ const Home = () => {
                 </Link>
 
                 <Link href={`/${language}/warehouse/in`} className='group'>
-                  <div className='bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
+                  <div className='h-full bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
                     <Truck className='text-green-500 w-6 h-6' />
                     <div>
                       <h2 className='font-medium group-hover:underline'>
@@ -59,7 +59,7 @@ const Home = () => {
                 </Link>
 
                 <Link href={`/${language}/warehouse/out`} className='group'>
-                  <div className='bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
+                  <div className='h-full bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
                     <Truck className='text-blue-500 w-6 h-6 rotate-180' />
                     <div>
                       <h2 className='font-medium group-hover:underline'>
@@ -76,7 +76,7 @@ const Home = () => {
                   href={`/${language}/warehouse/inventory`}
                   className='group'
                 >
-                  <div className='bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
+                  <div className='h-full bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
                     <List className='text-orange-500 w-6 h-6' />
                     <div>
                       <h2 className='font-medium group-hover:underline'>
@@ -90,7 +90,7 @@ const Home = () => {
                 </Link>
 
                 <Link href={`/${language}/warehouse/report`} className='group'>
-                  <div className='bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
+                  <div className='h-full bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
                     <BarChart className='text-purple-500 w-6 h-6' />
                     <div>
                       <h2 className='font-medium group-hover:underline'>
@@ -110,7 +110,7 @@ const Home = () => {
               <h2 className='text-2xl font-semibold mb-4'>{t('Home.other')}</h2>
               <div className='grid grid-cols-3 gap-4'>
                 <Link href={`/${language}/tasks`} className='group'>
-                  <div className='bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
+                  <div className='h-full bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
                     <Calendar className='text-blue-500 w-6 h-6' />
                     <div>
                       <h2 className='font-medium group-hover:underline'>
@@ -124,7 +124,7 @@ const Home = () => {
                 </Link>
 
                 <Link href={`/${language}/leave`} className='group'>
-                  <div className='bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
+                  <div className='h-full bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
                     <Clock className='text-green-500 w-6 h-6' />
                     <div>
                       <h2 className='font-medium group-hover:underline'>
@@ -138,7 +138,7 @@ const Home = () => {
                 </Link>
 
                 <Link href={`/${language}/attendance`} className='group'>
-                  <div className='bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
+                  <div className='h-full bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
                     <FileText className='text-orange-500 w-6 h-6' />
                     <div>
                       <h2 className='font-medium group-hover:underline'>
@@ -152,7 +152,7 @@ const Home = () => {
                 </Link>
 
                 <Link href={`/${language}/payroll`} className='group'>
-                  <div className='bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
+                  <div className='h-full bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4 hover:bg-gray-100 transition'>
                     <User className='text-purple-500 w-6 h-6' />
                     <div>
                       <h2 className='font-medium group-hover:underline'>
