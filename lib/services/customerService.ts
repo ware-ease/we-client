aa41683e333d7406ecc2f3d594f675a2c621fa35
conboaduide <vuvananhben26@gmail.com>
2025-03-16 15:35:27 +0700

    add customer and warehouse services

---
+++ lib/services/customerService.ts
@@ -0,0 +1,31 @@
+import { Customer } from '../types/customer';
+import { axiosDelete, axiosGet, axiosPost, axiosPut } from './baseService';
+
+export const getAllCustomers = async (): Promise<Customer[]> => {
+  const response = await axiosGet('/customers', {});
+  return response.data.data;
+};
+
+export const getCustomerById = async (id: string): Promise<Customer> => {
+  const response = await axiosGet(`/customers/${id}`, {});
+  return response.data.data;
+};
+
+export const createCustomer = async (
+  customerData: Partial<Customer>
+): Promise<Customer> => {
+  const response = await axiosPost('/customers', customerData, {});
+  return response.data.data;
+};
+
+export const updateCustomer = async (
+  id: string,
+  customerData: Partial<Customer>
+): Promise<Customer> => {
+  const response = await axiosPut(`/customers/${id}`, customerData, {});
+  return response.data.data;
+};
+
+export const deleteCustomer = async (id: string): Promise<void> => {
+  await axiosDelete(`/customers/${id}`, {});
+};
---
+++ lib/services/warehouseService.ts
@@ -0,0 +1,31 @@
+import { Warehouse } from '../types/warehouse';
+import { axiosDelete, axiosGet, axiosPatch, axiosPost } from './baseService';
+
+export const getAllWarehouses = async (): Promise<Warehouse[]> => {
+  const response = await axiosGet('/warehouses', {});
+  return response.data.data;
+};
+
+export const getWarehouseById = async (id: string): Promise<Warehouse> => {
+  const response = await axiosGet(`/warehouses/${id}`, {});
+  return response.data.data;
+};
+
+export const createWarehouse = async (
+  warehouseData: Partial<Warehouse>
+): Promise<Warehouse> => {
+  const response = await axiosPost('/warehouses', warehouseData, {});
+  return response.data.data;
+};
+
+export const updateWarehouse = async (
+  id: string,
+  warehouseData: Partial<Warehouse>
+): Promise<Warehouse> => {
+  const response = await axiosPatch(`/warehouses/${id}`, warehouseData, {});
+  return response.data.data;
+};
+
+export const deleteWarehouse = async (id: string): Promise<void> => {
+  await axiosDelete(`/warehouses/${id}`, {});
+};
