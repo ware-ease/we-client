fc8c95af60e90f502c31825c1ae3554333317b71
wintangz <wintang2003@icloud.com>
2025-03-22 01:22:10 +0700

    remove unused components

--- app/[locale]/(console)/accounts/page.tsx
+++ app/[locale]/(console)/accounts/page.tsx
@@ -4,7 +4,7 @@ export default function Accounts() {
   return (
     <div className='flex flex-col p-4 gap-6'>
       <div className='text-4xl font-semibold text-primary'>
-        Quản lý sản phẩm
+        Quản lý tài khoản
       </div>
       <AccountTable />
     </div>
