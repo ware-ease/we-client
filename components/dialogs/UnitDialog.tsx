a7234d1be9f7cf3976220e068de6616f8882a778
conboaduide <vuvananhben26@gmail.com>
2025-03-26 13:49:06 +0700

    update unit dialog

--- components/dialogs/UnitDialog.tsx
+++ components/dialogs/UnitDialog.tsx
@@ -3,9 +3,7 @@
 import { Button } from '@/components/shadcn-base/Button';
 import {
   Dialog,
-  DialogClose,
   DialogContent,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -13,15 +11,16 @@ import {
 import { Input } from '@/components/shadcn-base/Input';
 import { Label } from '@/components/shadcn-base/Label';
 import {
-  createUnit,
-  deleteUnit,
-  getAllUnits,
-  updateUnit,
-} from '@/services/unitService';
+  useAddUnit,
+  useDeleteUnit,
+  useUnits,
+  useUpdateUnit,
+} from '@/hooks/queries/unitQueries';
 import { Unit } from '@/types/unit';
-import { Check, Edit, Search, Trash2, X } from 'lucide-react';
+import { Edit, Search, Trash2, X } from 'lucide-react';
 import { ReactNode, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import { DeleteDialog } from './DeleteDialog';
 
 interface UnitDialogProps {
   children: ReactNode;
@@ -30,129 +29,134 @@ interface UnitDialogProps {
 const UnitDialog = ({ children }: UnitDialogProps) => {
   const [open, setOpen] = useState(false);
   const [showForm, setShowForm] = useState(false);
-  const [units, setUnits] = useState<Unit[]>([]);
-  const [filteredUnits, setFilteredUnits] = useState<Unit[]>([]);
-  const [newUnit, setNewUnit] = useState('');
+  const { data: units } = useUnits();
+  const [filteredUnits, setFilteredUnits] = useState<Unit[] | undefined>(
+    undefined
+  );
+  const [newUnit, setNewUnit] = useState<Partial<Unit>>({
+    name: '',
+    note: '',
+  });
   const [searchTerm, setSearchTerm] = useState('');
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
   const [editingUnit, setEditingUnit] = useState<string | null>(null);
-  const [editedName, setEditedName] = useState('');
-  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+  const [editedUnit, setEditedUnit] = useState<Partial<Unit>>({});
   const [unitToDelete, setUnitToDelete] = useState<Unit | null>(null);
+  const addUnitMutation = useAddUnit();
+  const updateUnitMutation = useUpdateUnit();
+  const deleteUnitMutation = useDeleteUnit();
 
   useEffect(() => {
-    if (open) fetchUnits();
-  }, [open]);
-
-  const fetchUnits = async () => {
-    try {
-      const data = await getAllUnits();
-      setUnits(data);
-      setFilteredUnits(data);
-    } catch (error) {
-      console.error('Lỗi khi tải danh sách đơn vị:', error);
-      toast.error('Không thể tải danh sách đơn vị.');
-    }
-  };
-
-  const handleAddUnit = async () => {
-    if (!newUnit.trim()) {
-      toast.error('Vui lòng nhập tên đơn vị hợp lệ.');
-      return;
-    }
-
-    try {
-      const createdUnit = await createUnit({ name: newUnit });
-      setUnits([...units, createdUnit]);
-      setFilteredUnits([...units, createdUnit]);
-      setNewUnit('');
-      setShowForm(false);
-      toast.success('Thêm đơn vị thành công!');
-    } catch (error) {
-      console.error('Lỗi khi thêm đơn vị:', error);
-      toast.error('Không thể thêm đơn vị.');
-    }
-  };
+    setFilteredUnits(units);
+  }, [units]);
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setSearchTerm(value);
     setFilteredUnits(
-      units.filter((unit) =>
+      units?.filter((unit) =>
         unit.name.toLowerCase().includes(value.toLowerCase())
       )
     );
   };
 
   const handleEditUnit = (unit: Unit) => {
-    setEditingUnit(unit.id);
-    setEditedName(unit.name);
+    setNewUnit(unit); // Gán unit hiện tại vào newUnit để chỉnh sửa
+    setShowForm(true); // Hiển thị form
   };
 
-  const handleSaveEdit = async (id: string) => {
-    try {
-      await updateUnit(id, { name: editedName });
-      const updatedUnits = units.map((unit) =>
-        unit.id === id ? { ...unit, name: editedName } : unit
+  const handleSaveUnit = () => {
+    if (!newUnit.name?.trim()) {
+      toast.error('Vui lòng nhập thông tin đơn vị hợp lệ.');
+      return;
+    }
+
+    if (newUnit.id) {
+      updateUnitMutation.mutate(
+        {
+          id: newUnit.id,
+          name: newUnit.name,
+          note: newUnit.note ?? '',
+        },
+        {
+          onSuccess: () => {
+            toast.success('Cập nhật đơn vị thành công!');
+            setShowForm(false);
+          },
+          onError: () => {
+            toast.error('Không thể cập nhật đơn vị.');
+          },
+        }
       );
-      setUnits(updatedUnits);
-      setFilteredUnits(updatedUnits);
-      setEditingUnit(null);
-      toast.success('Cập nhật đơn vị thành công!');
-    } catch (error) {
-      console.error('Lỗi khi cập nhật đơn vị:', error);
-      toast.error('Không thể cập nhật đơn vị.');
+    } else {
+      addUnitMutation.mutate({ ...newUnit } as Unit, {
+        onSuccess: () => {
+          toast.success('Thêm đơn vị thành công!');
+          setShowForm(false);
+          setNewUnit({ name: '', note: '' });
+        },
+        onError: () => {
+          toast.error('Không thể thêm đơn vị.');
+        },
+      });
     }
   };
 
   const handleCancelEdit = () => {
     setEditingUnit(null);
-    setEditedName('');
+    setEditedUnit({});
   };
 
-  const handleDeleteUnit = async () => {
+  const handleDeleteUnit = () => {
     if (!unitToDelete) return;
-    try {
-      await deleteUnit(unitToDelete?.id || '');
-      const updatedUnits = units.filter((unit) => unit.id !== unitToDelete.id);
-      setUnits(updatedUnits);
-      setFilteredUnits(updatedUnits);
-      toast.success('Xóa đơn vị thành công!');
-    } catch (error) {
-      console.error('Lỗi khi xóa đơn vị:', error);
-      toast.error('Không thể xóa đơn vị.');
-    } finally {
-      setDeleteDialogOpen(false);
-      setUnitToDelete(null);
-    }
+    deleteUnitMutation.mutate(unitToDelete.id!, {
+      onSuccess: () => {
+        toast.success('Xóa đơn vị thành công!');
+      },
+      onError: () => toast.error('Không thể xóa đơn vị.'),
+    });
   };
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>{children}</DialogTrigger>
-
-      <DialogContent className='w-full max-w-md bg-white p-6 rounded-lg shadow-lg'>
+      <DialogContent>
         <DialogHeader>
-          <DialogTitle className='text-lg font-semibold'>
-            Quản lý đơn vị
-          </DialogTitle>
+          <DialogTitle>Quản lý đơn vị</DialogTitle>
         </DialogHeader>
-
         {showForm ? (
           <div className='border p-4 rounded-md bg-gray-50'>
-            <Label htmlFor='unit-name' className='text-red-600'>
-              Tên đơn vị *
-            </Label>
-            <Input
-              id='unit-name'
-              value={newUnit}
-              onChange={(e) => setNewUnit(e.target.value)}
-              placeholder='Nhập đơn vị...'
-              className='mt-1'
-            />
+            <div>
+              <Label className='text-red-600'>Tên đơn vị *</Label>
+              <Input
+                value={newUnit.name}
+                onChange={(e) =>
+                  setNewUnit({ ...newUnit, name: e.target.value })
+                }
+                className='bg-white'
+              />
+            </div>
+            <div>
+              <Label>Ghi chú</Label>
+              <Input
+                value={newUnit.note}
+                onChange={(e) =>
+                  setNewUnit({ ...newUnit, note: e.target.value })
+                }
+                className='bg-white'
+              />
+            </div>
             <div className='flex justify-end mt-3 space-x-2'>
-              <Button onClick={handleAddUnit}>Lưu và chọn</Button>
-              <Button variant='secondary' onClick={() => setShowForm(false)}>
+              <Button onClick={handleSaveUnit}>
+                {newUnit.id ? 'Lưu thay đổi' : 'Lưu và chọn'}
+              </Button>
+              <Button
+                variant='secondary'
+                onClick={() => {
+                  setNewUnit({ name: '', note: '' });
+                  setShowForm(false);
+                }}
+              >
                 Hủy
               </Button>
             </div>
@@ -162,10 +166,9 @@ const UnitDialog = ({ children }: UnitDialogProps) => {
             className='w-full bg-green-500 text-white'
             onClick={() => setShowForm(true)}
           >
-            + Thêm đơn vị mới
+            + Tạo đơn vị mới
           </Button>
         )}
-
         <div className='relative'>
           <Search className='absolute left-3 top-2.5 text-gray-400' size={16} />
           <Input
@@ -176,14 +179,13 @@ const UnitDialog = ({ children }: UnitDialogProps) => {
             className='pl-9'
           />
         </div>
-
         <div className='border rounded-md p-3 max-h-60 overflow-auto'>
           <h3 className='text-sm font-semibold text-gray-600'>
             Danh sách đơn vị
           </h3>
           <ul className='mt-2 space-y-2'>
-            {filteredUnits.length > 0 ? (
-              filteredUnits.map((unit, index) => (
+            {filteredUnits?.length ?? 0 > 0 ? (
+              filteredUnits?.map((unit, index) => (
                 <li
                   key={unit.id}
                   className='flex justify-between items-center p-2 border rounded-md transition-all duration-300 hover:bg-gray-100'
@@ -193,13 +195,14 @@ const UnitDialog = ({ children }: UnitDialogProps) => {
                   {editingUnit === unit.id ? (
                     <div className='flex items-center space-x-2 w-full'>
                       <Input
-                        value={editedName}
-                        onChange={(e) => setEditedName(e.target.value)}
+                        value={editedUnit.name || ''}
+                        onChange={(e) =>
+                          setEditedUnit({
+                            ...editedUnit,
+                            name: e.target.value,
+                          })
+                        }
                         className='flex-1'
-                      />
-                      <Check
-                        className='text-green-600 h-5 w-5 cursor-pointer'
-                        onClick={() => handleSaveEdit(unit.id)}
                       />
                       <X
                         className='text-gray-600 h-5 w-5 cursor-pointer'
@@ -215,13 +218,19 @@ const UnitDialog = ({ children }: UnitDialogProps) => {
                             className='text-blue-600 h-4 w-4 cursor-pointer'
                             onClick={() => handleEditUnit(unit)}
                           />
-                          <Trash2
-                            className='text-red-600 h-4 w-4 cursor-pointer'
-                            onClick={() => {
-                              setUnitToDelete(unit);
-                              setDeleteDialogOpen(true);
-                            }}
-                          />
+                          <DeleteDialog
+                            onConfirmDelete={handleDeleteUnit}
+                            title='Xóa đơn vị'
+                            description='Bạn có chắc chắn muốn xóa đơn vị này không?'
+                            isLoading={deleteUnitMutation.isPending}
+                          >
+                            <Trash2
+                              className='text-red-600 h-4 w-4 cursor-pointer'
+                              onClick={() => {
+                                setUnitToDelete(unit);
+                              }}
+                            />
+                          </DeleteDialog>
                         </div>
                       )}
                     </>
@@ -230,41 +239,11 @@ const UnitDialog = ({ children }: UnitDialogProps) => {
               ))
             ) : (
               <p className='text-gray-500 text-sm text-center'>
-                Không có nhà cung cấp nào.
+                Không có đơn vị nào.
               </p>
             )}
           </ul>
         </div>
-        <Dialog open={deleteDialogOpen} onOpenChange={setDeleteDialogOpen}>
-          <DialogContent className='max-w-sm bg-white p-5 rounded-lg shadow-lg'>
-            <DialogHeader>
-              <DialogTitle className='text-red-600'>Xác nhận xóa</DialogTitle>
-            </DialogHeader>
-            <p>
-              Bạn có chắc chắn muốn xóa đơn vị &quot;
-              {unitToDelete?.name}&quot; không?
-            </p>
-            <DialogFooter>
-              <Button variant='destructive' onClick={handleDeleteUnit}>
-                Xóa
-              </Button>
-              <DialogClose asChild>
-                <Button variant='secondary'>Hủy</Button>
-              </DialogClose>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button
-              variant='secondary'
-              className='px-4 py-2 hover:bg-slate-200'
-            >
-              Đóng
-            </Button>
-          </DialogClose>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
--- hooks/queries/unitQueries.ts
+++ hooks/queries/unitQueries.ts
@@ -1,7 +1,17 @@
-import { createUnit, getAllUnits } from '@/services/unitService';
-import { Unit } from '@/types/unit';
+import {
+  createUnit,
+  deleteUnit,
+  getAllUnits,
+  updateUnit,
+} from '@/services/unitService';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
+
+export type Unit = {
+  id?: string;
+  name: string;
+  note: string;
+};
 
 export const useUnits = () =>
   useQuery({
@@ -16,13 +26,39 @@ export const useAddUnit = () => {
   return useMutation({
     mutationFn: (unit: Unit) => createUnit(unit),
     onSuccess: () => {
-      toast.success('Thành công!');
-      queryClient.invalidateQueries({
-        queryKey: ['units'],
-      });
+      toast.success('Thêm đơn vị thành công!');
+      queryClient.invalidateQueries({ queryKey: ['units'] });
     },
     onError: () => {
-      toast.error('Thất bại!');
+      toast.error('Không thể thêm đơn vị.');
+    },
+  });
+};
+
+export const useUpdateUnit = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ id, name, note }: Unit) => updateUnit(id!, { name, note }),
+    onSuccess: () => {
+      toast.success('Cập nhật đơn vị thành công!');
+      queryClient.invalidateQueries({ queryKey: ['units'] });
+    },
+    onError: () => {
+      toast.error('Không thể cập nhật đơn vị.');
+    },
+  });
+};
+
+export const useDeleteUnit = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (id: string) => deleteUnit(id),
+    onSuccess: () => {
+      toast.success('Xóa đơn vị thành công!');
+      queryClient.invalidateQueries({ queryKey: ['units'] });
+    },
+    onError: () => {
+      toast.error('Không thể xóa đơn vị.');
     },
   });
 };
