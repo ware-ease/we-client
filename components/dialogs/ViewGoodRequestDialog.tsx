f250a25a1b68987e11b35a1b2daab4129f866660
wintangz <wintang2003@icloud.com>
2025-04-16 01:06:31 +0700

    good notes carousel in view good request dialog

--- components/dialogs/ViewGoodRequestDialog.tsx
+++ components/dialogs/ViewGoodRequestDialog.tsx
@@ -8,9 +8,16 @@ import { Eye } from 'lucide-react';
 import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog';
 import { GoodRequest } from '@/types/goodRequest';
 import { Button } from '../shadcn-base/Button';
-import { useRef } from 'react';
+import { RefObject, useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import { useReactToPrint } from 'react-to-print';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '../shadcn-base/Carousel';
 
 interface GoodRequestDialogProps {
   goodRequest: GoodRequest;
@@ -23,11 +30,33 @@ export type GoodRequestDetail = {
 };
 
 export function ViewGoodRequestDialog({ goodRequest }: GoodRequestDialogProps) {
-  const contentRef = useRef(null);
+  // const contentRef = useRef(null);
 
-  // Function to handle printing/exporting to PDF
+  // // Function to handle printing/exporting to PDF
+  // const handlePrint = useReactToPrint({
+  //   contentRef,
+  // });
+
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+  const contentRefs = useRef<RefObject<HTMLDivElement | null>[]>([]);
+  const printRef = useRef<HTMLDivElement | null>(null);
+
+  // Initialize contentRefs
+  useEffect(() => {
+    const length = goodRequest.goodNotes?.length || 0;
+    contentRefs.current = Array.from({ length }, () => ({
+      current: null,
+    }));
+  }, [goodRequest.goodNotes]);
+
+  // Sync printRef with active slide
+  useEffect(() => {
+    printRef.current = contentRefs.current[activeSlideIndex]?.current || null;
+  }, [activeSlideIndex]);
+
+  // Handle printing for active slide
   const handlePrint = useReactToPrint({
-    contentRef,
+    contentRef: printRef,
   });
 
   // Determine request type
@@ -121,190 +150,195 @@ export function ViewGoodRequestDialog({ goodRequest }: GoodRequestDialogProps) {
 
         {/* Good Note Section (if exists) */}
         {goodRequest.goodNotes && goodRequest.goodNotes.length > 0 ? (
-          <div
-            ref={contentRef}
-            className='p-24 border-t border-gray-200 mt-6'
-            style={{ fontFamily: 'Times New Roman' }}
+          <Carousel
+            className='w-full max-w-3xl mx-auto'
+            setApi={(api) => {
+              api?.on('select', () => {
+                setActiveSlideIndex(api.selectedScrollSnap());
+              });
+            }}
           >
-            <div className='flex justify-between'>
-              <div className='w-1/2'>
-                <Image
-                  src='/TNTProjects.svg'
-                  alt='Company Logo'
-                  width={1080}
-                  height={1080}
-                  className='w-[45%] h-auto'
-                />
-              </div>
-              <div className='text-right'>
-                <p>
-                  <strong className='font-normal'>Số phiếu:</strong>{' '}
-                  {goodRequest.goodNotes[0]?.code || 'N/A'}
-                </p>
-                <p>
-                  <strong className='font-normal'>Ngày:</strong>{' '}
-                  {new Date(
-                    goodRequest.goodNotes[0]?.date || ''
-                  ).toLocaleDateString('vi-VN')}
-                </p>
-              </div>
-            </div>
+            <CarouselContent>
+              {goodRequest.goodNotes.map((goodNote, index) => (
+                <CarouselItem key={index} className=''>
+                  <div
+                    className='p-24 border-t border-gray-200 mt-6'
+                    ref={contentRefs.current[index]}
+                    style={{ fontFamily: 'Times New Roman' }}
+                  >
+                    <div className='flex justify-between'>
+                      <div className='w-1/2'>
+                        <Image
+                          src='/TNTProjects.svg'
+                          alt='Company Logo'
+                          width={1080}
+                          height={1080}
+                          className='w-[45%] h-auto'
+                        />
+                      </div>
+                      <div className='text-right'>
+                        <p>
+                          <strong className='font-normal'>Số phiếu:</strong>{' '}
+                          {goodNote.code || 'N/A'}
+                        </p>
+                        <p>
+                          <strong className='font-normal'>Ngày:</strong>{' '}
+                          {new Date(goodNote.date || '').toLocaleDateString(
+                            'vi-VN'
+                          )}
+                        </p>
+                      </div>
+                    </div>
 
-            <div>
-              {/* Title */}
-              <div className='text-center text-xl font-bold mt-20 text-[#2F5597]'>
-                {requestTitle.replace('YÊU CẦU', 'PHIẾU')}
-              </div>
+                    <div>
+                      {/* Title */}
+                      <div className='text-center text-xl font-bold mt-20 text-[#2F5597]'>
+                        {requestTitle.replace('YÊU CẦU', 'PHIẾU')}
+                      </div>
 
-              {/* Info Section */}
-              <div className='mt-4'>
-                {isInternalExportRequest ? (
-                  <>
-                    <p>
-                      <strong className='font-normal'>Kho xuất:</strong>{' '}
-                      {goodRequest.requestedWarehouse?.name || 'N/A'}
-                    </p>
-                    <p>
-                      <strong className='font-normal'>Địa chỉ:</strong>{' '}
-                      {goodRequest.requestedWarehouse?.address || 'N/A'}
-                    </p>
-                    <p>
-                      <strong className='font-normal'>SĐT:</strong>{' '}
-                      {goodRequest.requestedWarehouse?.phone || 'N/A'}
-                    </p>
-                    <p className='mt-2'>
-                      <strong className='font-normal'>Kho nhận:</strong>{' '}
-                      {goodRequest.warehouse?.name || 'N/A'}
-                    </p>
-                    <p>
-                      <strong className='font-normal'>Địa chỉ:</strong>{' '}
-                      {goodRequest.warehouse?.address || 'N/A'}
-                    </p>
-                    <p>
-                      <strong className='font-normal'>SĐT:</strong>{' '}
-                      {goodRequest.warehouse?.phone || 'N/A'}
-                    </p>
-                  </>
-                ) : (
-                  <>
-                    <p>
-                      <strong className='font-normal'>
-                        {isImportRequest ? 'Nhà cung cấp:' : ''}
-                      </strong>{' '}
-                      {isImportRequest
-                        ? goodRequest.partner?.name || 'N/A'
-                        : ''}
-                    </p>
-                    <p>
-                      <strong className='font-normal'>Địa chỉ:</strong>{' '}
-                      {goodRequest.requestedWarehouse?.address || 'N/A'}
-                    </p>
-                    <p>
-                      <strong className='font-normal'>SĐT:</strong>{' '}
-                      {goodRequest.requestedWarehouse?.phone || 'N/A'}
-                    </p>
-                  </>
-                )}
-              </div>
+                      {/* Info Section */}
+                      <div className='mt-4'>
+                        {isInternalExportRequest ? (
+                          <>
+                            <p>
+                              <strong className='font-normal'>Kho xuất:</strong>{' '}
+                              {goodRequest.requestedWarehouse?.name || 'N/A'}
+                            </p>
+                            <p>
+                              <strong className='font-normal'>Địa chỉ:</strong>{' '}
+                              {goodRequest.requestedWarehouse?.address || 'N/A'}
+                            </p>
+                            <p>
+                              <strong className='font-normal'>SĐT:</strong>{' '}
+                              {goodRequest.requestedWarehouse?.phone || 'N/A'}
+                            </p>
+                            <p className='mt-2'>
+                              <strong className='font-normal'>Kho nhận:</strong>{' '}
+                              {goodRequest.warehouse?.name || 'N/A'}
+                            </p>
+                            <p>
+                              <strong className='font-normal'>Địa chỉ:</strong>{' '}
+                              {goodRequest.warehouse?.address || 'N/A'}
+                            </p>
+                            <p>
+                              <strong className='font-normal'>SĐT:</strong>{' '}
+                              {goodRequest.warehouse?.phone || 'N/A'}
+                            </p>
+                          </>
+                        ) : (
+                          <>
+                            <p>
+                              <strong className='font-normal'>
+                                {isImportRequest ? 'Nhà cung cấp:' : ''}
+                              </strong>{' '}
+                              {isImportRequest
+                                ? goodRequest.partner?.name || 'N/A'
+                                : ''}
+                            </p>
+                            <p>
+                              <strong className='font-normal'>Địa chỉ:</strong>{' '}
+                              {goodRequest.requestedWarehouse?.address || 'N/A'}
+                            </p>
+                            <p>
+                              <strong className='font-normal'>SĐT:</strong>{' '}
+                              {goodRequest.requestedWarehouse?.phone || 'N/A'}
+                            </p>
+                          </>
+                        )}
+                      </div>
 
-              {/* Good Note Table */}
-              <table className='w-full mt-4 border border-black'>
-                <thead>
-                  <tr className='text-left font-normal'>
-                    <th className='border-r border-black p-2 font-normal'>
-                      STT
-                    </th>
-                    <th className='border-r border-black p-2 font-normal'>
-                      Mã hàng
-                    </th>
-                    <th className='border-r border-black p-2 font-normal'>
-                      Tên hàng
-                    </th>
-                    <th className='border-r border-black p-2 font-normal'>
-                      ĐVT
-                    </th>
-                    <th className='border-r border-black p-2 font-normal'>
-                      Số lượng
-                    </th>
-                    <th className='p-2 font-normal'>Ghi chú</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {goodRequest.goodNotes[0]?.goodNoteDetails?.map(
-                    (detail, index) => (
-                      <tr key={index}>
-                        <td className='border-r border-t border-black p-2'>
-                          {index + 1}
-                        </td>
-                        <td className='border-r border-t border-black p-2'>
-                          {detail.batch?.product?.sku || 'N/A'}
-                        </td>
-                        <td className='border-r border-t border-black p-2'>
-                          {detail.batch?.product?.name || 'N/A'}
-                        </td>
-                        <td className='border-r border-t border-black p-2'>
-                          {detail.batch?.product?.unitName || 'N/A'}
-                        </td>
-                        <td className='border-r border-t border-black p-2'>
-                          {detail.quantity}
-                        </td>
-                        <td className='border-t border-black p-2'>
-                          {detail.note || ''}
-                        </td>
-                      </tr>
-                    )
-                  )}
-                </tbody>
-              </table>
+                      {/* Good Note Table */}
+                      <table className='w-full mt-4 border border-black'>
+                        <thead>
+                          <tr className='text-left font-normal'>
+                            <th className='border-r border-black p-2 font-normal'>
+                              STT
+                            </th>
+                            <th className='border-r border-black p-2 font-normal'>
+                              Mã hàng
+                            </th>
+                            <th className='border-r border-black p-2 font-normal'>
+                              Tên hàng
+                            </th>
+                            <th className='border-r border-black p-2 font-normal'>
+                              ĐVT
+                            </th>
+                            <th className='border-r border-black p-2 font-normal'>
+                              Số lượng
+                            </th>
+                            <th className='p-2 font-normal'>Ghi chú</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {goodNote.goodNoteDetails?.map((detail, index) => (
+                            <tr key={index}>
+                              <td className='border-r border-t border-black p-2'>
+                                {index + 1}
+                              </td>
+                              <td className='border-r border-t border-black p-2'>
+                                {detail.batch?.product?.sku || 'N/A'}
+                              </td>
+                              <td className='border-r border-t border-black p-2'>
+                                {detail.batch?.product?.name || 'N/A'}
+                              </td>
+                              <td className='border-r border-t border-black p-2'>
+                                {detail.batch?.product?.unitName || 'N/A'}
+                              </td>
+                              <td className='border-r border-t border-black p-2'>
+                                {detail.quantity}
+                              </td>
+                              <td className='border-t border-black p-2'>
+                                {detail.note || ''}
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
 
-              {/* Footer */}
-              <div className='flex justify-end mt-10 mr-6'>
-                <div>Ngày...Tháng...Năm...</div>
-              </div>
-              <div className='flex justify-between mt-2'>
-                <div className='text-center w-1/2'>
-                  <p>
-                    <strong>
-                      {isInternalExportRequest ? 'THỦ KHO XUẤT' : 'THỦ KHO'}
-                    </strong>
-                  </p>
-                  <p>(Ký, họ tên)</p>
-                </div>
-                <div className='text-center w-1/2'>
-                  <p>
-                    <strong>
-                      {isImportRequest
-                        ? 'NGƯỜI GIAO HÀNG'
-                        : isExportRequest
-                        ? 'NGƯỜI NHẬN HÀNG'
-                        : 'THỦ KHO NHẬN'}
-                    </strong>
-                  </p>
-                  <p>(Ký, họ tên)</p>
-                </div>
-              </div>
-            </div>
-          </div>
+                      {/* Footer */}
+                      <div className='flex justify-end mt-10 mr-6'>
+                        <div>Ngày...Tháng...Năm...</div>
+                      </div>
+                      <div className='flex justify-between mt-2'>
+                        <div className='text-center w-1/2'>
+                          <p>
+                            <strong>
+                              {isInternalExportRequest
+                                ? 'THỦ KHO XUẤT'
+                                : 'THỦ KHO'}
+                            </strong>
+                          </p>
+                          <p>(Ký, họ tên)</p>
+                        </div>
+                        <div className='text-center w-1/2'>
+                          <p>
+                            <strong>
+                              {isImportRequest
+                                ? 'NGƯỜI GIAO HÀNG'
+                                : isExportRequest
+                                ? 'NGƯỜI NHẬN HÀNG'
+                                : 'THỦ KHO NHẬN'}
+                            </strong>
+                          </p>
+                          <p>(Ký, họ tên)</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <Button className='w-full' onClick={() => handlePrint()}>
+                    In phiếu
+                  </Button>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         ) : (
           <div className='flex flex-col items-center p-6'>
             <p className='text-lg text-gray-600'>{noNoteMessage}</p>
           </div>
         )}
-
-        {/* Buttons */}
-        <div className='flex space-x-4 mt-4 px-6'>
-          {goodRequest.goodNotes && goodRequest.goodNotes.length > 0 ? (
-            <>
-              <Button className='w-full' onClick={() => handlePrint()}>
-                In phiếu
-              </Button>
-            </>
-          ) : (
-            <Button className='w-full' disabled>
-              {noNoteMessage}
-            </Button>
-          )}
-        </div>
       </DialogContent>
     </Dialog>
   );
