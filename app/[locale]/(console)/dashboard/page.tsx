50a5b53080390feaf0e93e4a61b1b815258a2ea2
conboaduide <vuvananhben26@gmail.com>
2025-04-21 19:12:31 +0700

    api card

--- app/[locale]/(console)/dashboard/page.tsx
+++ app/[locale]/(console)/dashboard/page.tsx
@@ -3,45 +3,97 @@ import { AreaCharts } from '@/components/dashboards/AreaChart';
 import BaseCard from '@/components/dashboards/BaseCard';
 import { PieCharts } from '@/components/dashboards/PieChart';
 import { StockChart } from '@/components/dashboards/StockChart';
+import { getDashboardCards } from '@/services/dashboard';
 import {
   LucideArrowDownToLine,
   LucideArrowRightLeft,
   LucideArrowUpFromLine,
   LucidePackage,
 } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 const Dashboard = () => {
+  const [cardData, setCardData] = useState<{
+    totalPutIn: number;
+    changePutIn: number;
+    totalTakeOut: number;
+    changeTakeOut: number;
+    currentStockChange: number;
+    changeStock: number;
+    totalTransfer: number;
+    changeTransfer: number;
+  } | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getDashboardCards();
+        setCardData(response);
+      } catch (error) {
+        console.error('Error fetching dashboard cards:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className='grid gap-6 p-6 sm:grid-cols-2 lg:grid-cols-4'>
       {/* Tổng nhập kho trong tháng */}
       <BaseCard
         title='Tổng nhập kho trong tháng'
-        amount='8,500 mặt hàng'
-        percentageChange='Tăng 3% so với tháng trước'
+        amount={
+          cardData ? `${cardData.totalPutIn.toLocaleString()} mặt hàng` : '...'
+        }
+        percentageChange={
+          cardData ? `Tăng ${cardData.changePutIn}% so với tháng trước` : '...'
+        }
         icon={LucideArrowDownToLine}
         iconClass='text-green-500'
       />
       {/* Tổng xuất kho trong tháng */}
       <BaseCard
         title='Tổng xuất kho trong tháng'
-        amount='9,200 mặt hàng'
-        percentageChange='Giảm 2% so với tháng trước'
+        amount={
+          cardData
+            ? `${cardData.totalTakeOut.toLocaleString()} mặt hàng`
+            : '...'
+        }
+        percentageChange={
+          cardData
+            ? `Tăng ${cardData.changeTakeOut}% so với tháng trước`
+            : '...'
+        }
         icon={LucideArrowUpFromLine}
         iconClass='text-yellow-500'
       />
       {/* Tổng tồn kho hiện tại */}
       <BaseCard
         title='Tổng tồn kho hiện tại'
-        amount='12,300 mặt hàng'
-        percentageChange='Giảm 5% so với đầu tháng'
+        amount={
+          cardData
+            ? `${cardData.currentStockChange.toLocaleString()} mặt hàng`
+            : '...'
+        }
+        percentageChange={
+          cardData ? `Tăng ${cardData.changeStock}% so với đầu tháng` : '...'
+        }
         icon={LucidePackage}
         iconClass='text-blue-500'
       />
       {/* Tổng chuyển kho trong tháng */}
       <BaseCard
         title='Tổng chuyển kho trong tháng'
-        amount='1,200 mặt hàng'
-        percentageChange='Tăng 10% so với tháng trước'
+        amount={
+          cardData
+            ? `${cardData.totalTransfer.toLocaleString()} mặt hàng`
+            : '...'
+        }
+        percentageChange={
+          cardData
+            ? `Tăng ${cardData.changeTransfer}% so với tháng trước`
+            : '...'
+        }
         icon={LucideArrowRightLeft}
         iconClass='text-purple-500'
       />
